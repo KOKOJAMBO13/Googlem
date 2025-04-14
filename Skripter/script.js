51aeccdcd7e85a9c7060b7ef793539e95783e275
KOKOJAMBO13 <indexhtml1css2@gmail.com>
2025-04-14 13:26:28 +0500

    Add files via upload

---
+++ Skripter/script.js
@@ -0,0 +1,30 @@
+let mars = "heelo"
+
+console.log(typeof mar
+let mmm = 123
+
+console.log(typeof mmm
+let aaa = false
+
+console.log( typeof true
+let bbb = 123n
+
+console.log(typeof bbb
+let ggg = null
+
+console.log(typeof ggg);
+let frfrfr = symbol
+
+let strng = prompt('String тфйп')
+
+let num = prompt('Number тайп')
+
+let bl = prompt('boolean тайпg')
+
+confirm('Всё точно правильно')
+
+let = alert(`Siz kiritgan ${strng} sozi ${typeof(strng)} typega tegishli ` + `Siz kiritgan ${num} sozi  number typega tegishli` + `Siz kiritgan ${bl} sozi ${typeof(true)} typega tegishli`)
+
+
+
+
